efc62f11199951e4f6ffdd92cc474a40e194b241
keepgoing228 <keepdling@gmail.com>
2017-08-08 00:53:31 +0800

    第一講範例

---
+++ Lecture01-Examples/Example06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sq1 =
+                @"
+                 select* from table
+                 where id > 10 
+                 ";
+            Console.WriteLine(sq1);
+
+            
+
+        }
+    }
+}
---
+++ Lecture01-Examples/pratice01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratice01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "bulbasaur";
+            int nationNo = 1;
+            string species = "Seed Pokémon";
+            float Height = 0.71f;
+            float Weight = 6.9f;
+            string Abilities = "Overgrow Chlorophyll(hidden ability)";
+            string LocalNo = "001 (Red/Blue/Yellow/FireRed/LeafGreen) 226(Gold / Silver / Crystal) 231(HeartGold / SoulSilver) 080(X / Y)";
+            string Japanese = "Fushigidane";
+
+            Console.WriteLine("Pokemon Name:{0}",name);
+            Console.WriteLine("National No:{0}", nationNo);
+            Console.WriteLine("species:{0}", species); Console.WriteLine("Pokemon Name:{0}", name);
+            Console.WriteLine("Height:{0}", Height);
+            Console.WriteLine("Weight:{0}", Weight);
+            Console.WriteLine("Abilities:{0}", Abilities);
+            Console.WriteLine("Local No:{0}", LocalNo);
+            Console.WriteLine("Japanese:{0}", Japanese);
+
+
+        }
+    }
+}
